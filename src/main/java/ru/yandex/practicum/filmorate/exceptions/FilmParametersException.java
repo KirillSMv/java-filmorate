c7237686c0abb0a3cbe04ab4dba8eec73e14e7f0
Kirill <kirill551@yandex.ru>
2023-11-01 09:22:44 +0300

    refactor

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmParametersException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class FilmParametersException extends RuntimeException {
-    public FilmParametersException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/yandex/practicum/filmorate/exceptions/UserParametersException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class UserParametersException extends RuntimeException {
-    public UserParametersException(String message) {
-        super(message);
-    }
-}
