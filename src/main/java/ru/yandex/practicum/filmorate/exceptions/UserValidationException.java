63f27acb82a05da3d3ba42382a6493c87466736d
Kirill <kirill551@yandex.ru>
2023-11-02 20:11:43 +0300

    style: удалить неиспользуемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/UserValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/UserValidationException.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-import java.time.LocalDate;
-
 public class UserValidationException extends RuntimeException {
 
     public UserValidationException(String message) {
