96bf94d782dc6fa6f646bcdfa6ba9e9d40d01e81
Kirill <kirill551@yandex.ru>
2023-11-20 19:54:25 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/controllers/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/ErrorHandler.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.yandex.practicum.filmorate.exceptions.*;
 import ru.yandex.practicum.filmorate.model.ErrorResponse;
